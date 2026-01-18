e641dee3205c264b6b4e1a653bb0b41340961fb1
Irfan <irfan-dahir@users.noreply.github.com>
2026-01-18 07:50:21 +0500

    Update .php-cs-fixer.dist.php

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,11 +4,11 @@ $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__.'/src')
 ;
 
-return (new PhpCsFixer\Config())
+return new PhpCsFixer\Config()
     ->setRules([
         '@PSR12' => true,
         '@PER-CS' => true,
-        '@PHP82Migration' => true,
+        '@PHP84Migration' => true,
         'array_syntax' => ['syntax' => 'short'],
     ])
     ->setFinder($finder)
